796bf28f725763c9405292d90594aaa990c5fc4d
Cryslayne Cinara <cryslaynecinara0231@gmail.com>
2026-04-05 17:45:33 -0300

    feat: export app instance from new router index file

--- server/routers/index.ts
+++ server/routers/index.ts
@@ -1,14 +1,2 @@
-import { router } from "../../../server/_core/trpc";
-import { authRouter } from "./auth";
-import { configRouter } from "./config";
-import { surveysRouter } from "./surveys";
-import { csvRouter } from "./csv";
-
-export const appRouter = router({
-    auth: authRouter,
-    config: configRouter,
-    surveys: surveysRouter,
-    csv: csvRouter,
-});
-
-export type AppRouter = typeof appRouter;
+import app from '../_core/index.js';
+export default app;
